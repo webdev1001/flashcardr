578f13ad13e1f34143519b880919add989ee413f
webdev1001 <arian@devtrend.ru>
2016-02-02 20:23:15 +0300

    generate Card model

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap.min
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
