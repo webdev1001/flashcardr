4db1d64c226a19ce82ba8f95e8209eb047f76ce0
webdev1001 <arian@devtrend.ru>
2016-02-17 15:38:20 +0300

    Added Validations and New/Edit/Show pages

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,51 @@
 class CardsController < ApplicationController
+	before_action :card_init, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@cards = Card.all
 	end
+
+	def show
+	end
+
+	def new
+		@card = Card.new
+	end
+
+	def edit
+	end
+
+	def create
+		@card = Card.create(card_params)
+		
+		if @card.save
+			redirect_to @card
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @card.update(card_params)
+			redirect_to @card
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@card.destroy
+		redirect_to cards_path
+	end
+
+	private
+
+		def card_init
+			@card = Card.find(params[:id])			
+		end
+
+		def card_params
+    	params.require(:card).permit(:id, :original_text, :translated_text, :review_date)
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'  
   #get 'cards', to: 'cards#index'
-  resources :cards, only: :index
+  resources :cards
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
