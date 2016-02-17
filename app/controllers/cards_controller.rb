0b92da5deeb39390305b55ff8690ea6b92fb69cf
webdev1001 <arian@devtrend.ru>
2016-02-17 16:19:12 +0300

    Fixed 2 spaces styling in card_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,51 +1,51 @@
 class CardsController < ApplicationController
-	before_action :card_init, only: [:show, :edit, :update, :destroy]
+  before_action :card_init, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@cards = Card.all
-	end
+  def index
+    @cards = Card.all
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@card = Card.new
-	end
+  def new
+    @card = Card.new
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def create
-		@card = Card.create(card_params)
+  def create
+    @card = Card.create(card_params)
 		
-		if @card.save
-			redirect_to @card
-		else
-			render 'new'
-		end
-	end
+    if @card.save
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		if @card.update(card_params)
-			redirect_to @card
-		else
-			render 'edit'
-		end
-	end
+  def update
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@card.destroy
-		redirect_to cards_path
-	end
+  def destroy
+    @card.destroy
+    redirect_to cards_path
+  end
 
-	private
+  private
 
-		def card_init
-			@card = Card.find(params[:id])			
-		end
+    def card_init
+      @card = Card.find(params[:id])			
+    end
 
-		def card_params
-    	params.require(:card).permit(:id, :original_text, :translated_text, :review_date)
-  	end
+    def card_params
+      params.require(:card).permit(:id, :original_text, :translated_text, :review_date)
+    end
 
 end
