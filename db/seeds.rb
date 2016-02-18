899dcb17190e281657b18b2ccd7bcdb012934c2d
webdev1001 <arian@devtrend.ru>
2016-02-18 21:58:02 +0300

    Added Nokogiri parsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'open-uri'
+
+html = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
+doc = Nokogiri::HTML(open(html))
+
+table_rows = doc.css("tr")
+table_rows.shift # Remove first element (row) from NodeSet
+
+table_rows.each do |row|
+  original_text = row.first_element_child.next_element.inner_text
+  translated_text = row.first_element_child.next_element.next_element.inner_text
+  Card.create(original_text: original_text, translated_text:translated_text, review_date: Time.now)
+end
