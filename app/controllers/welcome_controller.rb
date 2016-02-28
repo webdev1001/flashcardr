961ad1178621baaa0f15dcd8a57ff83ac8c4c56c
webdev1001 <arian@devtrend.ru>
2016-02-28 15:44:34 +0300

    Added feature test for check translation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,19 +2,21 @@ class WelcomeController < ApplicationController
 
   def index
     @card = Card.card_for_review.first
-  end  
+  end
 
   def check_translation
     card = Card.find(params[:welcome][:id])
+    correct_translation_message = "Правильный перевод! Переведите следующую карточку"
+    wrong_translation_message = "Перевод неверен! Потренируйтесь еще."
     if card.check_translation(params[:welcome][:user_translation_text])
       card.set_review_date
       card.save!
-      flash[:message] = "Правильный перевод! Переведите следующую карточку"
+      flash[:message] = correct_translation_message
     else
-      flash[:message] = "Перевод неверен! Потренируйтесь еще."
+      flash[:message] = wrong_translation_message
     end
 
-    redirect_to :back
+    redirect_to root_path #:back
   end
 
 end
---
+++ spec/features/welcome_translation_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+feature "Translation Check" do
+  scenario "is correct on welcome page" do
+    given_there_is_a_card
+    when_i_check_correct_translation_on_welcome_page
+    then_success_message_should_appear
+  end
+
+  scenario "is wrong on welcome page" do
+    given_there_is_a_card
+    when_i_check_wrong_translation_on_welcome_page
+    then_false_message_should_appear
+  end
+
+  def given_there_is_a_card
+    @card = create(:card)
+    @card.review_date = Time.now.utc - 1.day # to pass check to display card
+    @card.save!
+  end
+
+  def when_i_check_correct_translation_on_welcome_page
+    visit root_path
+    fill_in "welcome_user_translation_text", with: @card.original_text
+    click_button 'Проверить перевод'
+  end
+
+  def when_i_check_wrong_translation_on_welcome_page
+    visit root_path
+    fill_in "welcome_user_translation_text", with: @card.translated_text
+    click_button 'Проверить перевод'
+  end
+
+  def then_success_message_should_appear
+    expect(page).to have_content :correct_translation_message
+  end
+
+  def then_false_message_should_appear
+    expect(page).to have_content :wrong_translation_message
+  end
+end
